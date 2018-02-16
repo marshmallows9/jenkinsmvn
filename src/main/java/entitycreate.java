71ce5603a44703fdf2e71f9aa7434076d60f2e75
julia <juliea_robort@yahoo.co.in>
2018-02-15 19:39:06 -0800

    test

--- src/main/java/entitycreate.java
+++ src/main/java/entitycreate.java
@@ -11,7 +11,7 @@ import com.github.javafaker.Faker;
 public class entitycreate {
 
         public static void main(String[] args) throws InterruptedException{
-            System.setProperty("webdriver.chrome.driver", "/Users/suchismitasethi/Downloads/chromedriver");
+            System.setProperty("webdriver.chrome.driver", "/Users/Shared/Jenkins/Home/workspace/WavefitsAutomation/chromedriver");
             WebDriver dr = new ChromeDriver();
 
             Faker faker = new Faker();
